9666f1881b4471597f2750745a965ea2e2bdb4fe
FacePunch1337 <rezol1337@gmail.com>
2022-04-21 16:24:46 +0200

    Добавьте файлы проекта.

---
+++ DotNetHooks/HookForm.Designer.cs
@@ -0,0 +1,324 @@
+﻿
+namespace DotNetHooks
+{
+    partial class HookForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabPageKeyboard = new System.Windows.Forms.TabPage();
+            this.buttonStopKb = new System.Windows.Forms.Button();
+            this.richTextBoxKbDemo = new System.Windows.Forms.RichTextBox();
+            this.buttonStartKb = new System.Windows.Forms.Button();
+            this.richTextBoxKb = new System.Windows.Forms.RichTextBox();
+            this.tabPageMouse = new System.Windows.Forms.TabPage();
+            this.panelDisplay = new System.Windows.Forms.Panel();
+            this.buttonStopMs = new System.Windows.Forms.Button();
+            this.buttonStartMs = new System.Windows.Forms.Button();
+            this.listBoxMs = new System.Windows.Forms.ListBox();
+            this.kbHookStatus = new System.Windows.Forms.Label();
+            this.msHookStatus = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelRes = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBoxReplaces = new System.Windows.Forms.ListBox();
+            this.textBoxSourceKey = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonAddReplace = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tabControl.SuspendLayout();
+            this.tabPageKeyboard.SuspendLayout();
+            this.tabPageMouse.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.tabPageKeyboard);
+            this.tabControl.Controls.Add(this.tabPageMouse);
+            this.tabControl.Location = new System.Drawing.Point(301, 12);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(487, 426);
+            this.tabControl.TabIndex = 0;
+            // 
+            // tabPageKeyboard
+            // 
+            this.tabPageKeyboard.Controls.Add(this.groupBox1);
+            this.tabPageKeyboard.Controls.Add(this.buttonStopKb);
+            this.tabPageKeyboard.Controls.Add(this.richTextBoxKbDemo);
+            this.tabPageKeyboard.Controls.Add(this.buttonStartKb);
+            this.tabPageKeyboard.Controls.Add(this.richTextBoxKb);
+            this.tabPageKeyboard.Location = new System.Drawing.Point(4, 22);
+            this.tabPageKeyboard.Name = "tabPageKeyboard";
+            this.tabPageKeyboard.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageKeyboard.Size = new System.Drawing.Size(479, 400);
+            this.tabPageKeyboard.TabIndex = 0;
+            this.tabPageKeyboard.Text = "Keyboard";
+            this.tabPageKeyboard.UseVisualStyleBackColor = true;
+            // 
+            // buttonStopKb
+            // 
+            this.buttonStopKb.Location = new System.Drawing.Point(87, 6);
+            this.buttonStopKb.Name = "buttonStopKb";
+            this.buttonStopKb.Size = new System.Drawing.Size(75, 23);
+            this.buttonStopKb.TabIndex = 2;
+            this.buttonStopKb.Text = "Stop";
+            this.buttonStopKb.UseVisualStyleBackColor = true;
+            this.buttonStopKb.Click += new System.EventHandler(this.buttonStopKb_Click);
+            // 
+            // richTextBoxKbDemo
+            // 
+            this.richTextBoxKbDemo.Location = new System.Drawing.Point(227, 87);
+            this.richTextBoxKbDemo.Name = "richTextBoxKbDemo";
+            this.richTextBoxKbDemo.Size = new System.Drawing.Size(246, 130);
+            this.richTextBoxKbDemo.TabIndex = 1;
+            this.richTextBoxKbDemo.Text = "";
+            // 
+            // buttonStartKb
+            // 
+            this.buttonStartKb.Location = new System.Drawing.Point(6, 6);
+            this.buttonStartKb.Name = "buttonStartKb";
+            this.buttonStartKb.Size = new System.Drawing.Size(75, 23);
+            this.buttonStartKb.TabIndex = 1;
+            this.buttonStartKb.Text = "Start";
+            this.buttonStartKb.UseVisualStyleBackColor = true;
+            this.buttonStartKb.Click += new System.EventHandler(this.buttonStartKb_Click);
+            // 
+            // richTextBoxKb
+            // 
+            this.richTextBoxKb.Location = new System.Drawing.Point(227, 6);
+            this.richTextBoxKb.Name = "richTextBoxKb";
+            this.richTextBoxKb.Size = new System.Drawing.Size(246, 78);
+            this.richTextBoxKb.TabIndex = 0;
+            this.richTextBoxKb.Text = "";
+            // 
+            // tabPageMouse
+            // 
+            this.tabPageMouse.Controls.Add(this.labelRes);
+            this.tabPageMouse.Controls.Add(this.panelDisplay);
+            this.tabPageMouse.Controls.Add(this.buttonStopMs);
+            this.tabPageMouse.Controls.Add(this.buttonStartMs);
+            this.tabPageMouse.Controls.Add(this.listBoxMs);
+            this.tabPageMouse.Location = new System.Drawing.Point(4, 22);
+            this.tabPageMouse.Name = "tabPageMouse";
+            this.tabPageMouse.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageMouse.Size = new System.Drawing.Size(479, 400);
+            this.tabPageMouse.TabIndex = 1;
+            this.tabPageMouse.Text = "Mouse";
+            this.tabPageMouse.UseVisualStyleBackColor = true;
+            // 
+            // panelDisplay
+            // 
+            this.panelDisplay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelDisplay.Location = new System.Drawing.Point(209, 6);
+            this.panelDisplay.Name = "panelDisplay";
+            this.panelDisplay.Size = new System.Drawing.Size(254, 147);
+            this.panelDisplay.TabIndex = 5;
+            // 
+            // buttonStopMs
+            // 
+            this.buttonStopMs.Location = new System.Drawing.Point(6, 35);
+            this.buttonStopMs.Name = "buttonStopMs";
+            this.buttonStopMs.Size = new System.Drawing.Size(75, 23);
+            this.buttonStopMs.TabIndex = 4;
+            this.buttonStopMs.Text = "Stop";
+            this.buttonStopMs.UseVisualStyleBackColor = true;
+            this.buttonStopMs.Click += new System.EventHandler(this.buttonStopMs_Click);
+            // 
+            // buttonStartMs
+            // 
+            this.buttonStartMs.Location = new System.Drawing.Point(6, 6);
+            this.buttonStartMs.Name = "buttonStartMs";
+            this.buttonStartMs.Size = new System.Drawing.Size(75, 23);
+            this.buttonStartMs.TabIndex = 3;
+            this.buttonStartMs.Text = "Start";
+            this.buttonStartMs.UseVisualStyleBackColor = true;
+            this.buttonStartMs.Click += new System.EventHandler(this.buttonStartMs_Click);
+            // 
+            // listBoxMs
+            // 
+            this.listBoxMs.FormattingEnabled = true;
+            this.listBoxMs.Location = new System.Drawing.Point(87, 6);
+            this.listBoxMs.Name = "listBoxMs";
+            this.listBoxMs.Size = new System.Drawing.Size(97, 147);
+            this.listBoxMs.TabIndex = 0;
+            // 
+            // kbHookStatus
+            // 
+            this.kbHookStatus.AutoSize = true;
+            this.kbHookStatus.ForeColor = System.Drawing.Color.Red;
+            this.kbHookStatus.Location = new System.Drawing.Point(66, 34);
+            this.kbHookStatus.Name = "kbHookStatus";
+            this.kbHookStatus.Size = new System.Drawing.Size(57, 13);
+            this.kbHookStatus.TabIndex = 1;
+            this.kbHookStatus.Text = "non-active";
+            // 
+            // msHookStatus
+            // 
+            this.msHookStatus.AutoSize = true;
+            this.msHookStatus.ForeColor = System.Drawing.Color.Red;
+            this.msHookStatus.Location = new System.Drawing.Point(66, 69);
+            this.msHookStatus.Name = "msHookStatus";
+            this.msHookStatus.Size = new System.Drawing.Size(57, 13);
+            this.msHookStatus.TabIndex = 2;
+            this.msHookStatus.Text = "non-active";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Keyboard: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Mouse: ";
+            // 
+            // labelRes
+            // 
+            this.labelRes.AutoSize = true;
+            this.labelRes.Location = new System.Drawing.Point(206, 167);
+            this.labelRes.Name = "labelRes";
+            this.labelRes.Size = new System.Drawing.Size(22, 13);
+            this.labelRes.TabIndex = 5;
+            this.labelRes.Text = "[...]";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.buttonAddReplace);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBoxSourceKey);
+            this.groupBox1.Controls.Add(this.listBoxReplaces);
+            this.groupBox1.Location = new System.Drawing.Point(6, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(215, 182);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Replaces";
+            // 
+            // listBoxReplaces
+            // 
+            this.listBoxReplaces.FormattingEnabled = true;
+            this.listBoxReplaces.Location = new System.Drawing.Point(7, 19);
+            this.listBoxReplaces.Name = "listBoxReplaces";
+            this.listBoxReplaces.Size = new System.Drawing.Size(196, 108);
+            this.listBoxReplaces.TabIndex = 0;
+            // 
+            // textBoxSourceKey
+            // 
+            this.textBoxSourceKey.Location = new System.Drawing.Point(7, 139);
+            this.textBoxSourceKey.Name = "textBoxSourceKey";
+            this.textBoxSourceKey.Size = new System.Drawing.Size(49, 20);
+            this.textBoxSourceKey.TabIndex = 1;
+            this.textBoxSourceKey.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxSourceKey_KeyDown);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(96, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "-->";
+            // 
+            // buttonAddReplace
+            // 
+            this.buttonAddReplace.Location = new System.Drawing.Point(86, 158);
+            this.buttonAddReplace.Name = "buttonAddReplace";
+            this.buttonAddReplace.Size = new System.Drawing.Size(39, 22);
+            this.buttonAddReplace.TabIndex = 4;
+            this.buttonAddReplace.Text = "+";
+            this.buttonAddReplace.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(154, 139);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(49, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxSourceKey_KeyDown);
+            // 
+            // HookForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.msHookStatus);
+            this.Controls.Add(this.kbHookStatus);
+            this.Controls.Add(this.tabControl);
+            this.Name = "HookForm";
+            this.Text = "HookForm";
+            this.Load += new System.EventHandler(this.HookForm_Load);
+            this.tabControl.ResumeLayout(false);
+            this.tabPageKeyboard.ResumeLayout(false);
+            this.tabPageMouse.ResumeLayout(false);
+            this.tabPageMouse.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabPageKeyboard;
+        private System.Windows.Forms.TabPage tabPageMouse;
+        private System.Windows.Forms.RichTextBox richTextBoxKb;
+        private System.Windows.Forms.Button buttonStartKb;
+        private System.Windows.Forms.Button buttonStopKb;
+        private System.Windows.Forms.RichTextBox richTextBoxKbDemo;
+        private System.Windows.Forms.Button buttonStopMs;
+        private System.Windows.Forms.Button buttonStartMs;
+        private System.Windows.Forms.ListBox listBoxMs;
+        private System.Windows.Forms.Label kbHookStatus;
+        private System.Windows.Forms.Label msHookStatus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panelDisplay;
+        private System.Windows.Forms.Label labelRes;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button buttonAddReplace;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxSourceKey;
+        private System.Windows.Forms.ListBox listBoxReplaces;
+    }
+}
---
+++ DotNetHooks/HookForm.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DotNetHooks
+{
+    public partial class HookForm : Form
+    {
+
+        
+
+        private delegate IntPtr HookProc(int nCode, IntPtr wParam, IntPtr lParam);
+        #region DLL Import
+        [DllImport("user32.dll", EntryPoint = "SetWindowsHookEx", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetHook(int idHook, HookProc lpfnProc, IntPtr hMod, uint dwThreadId);
+
+
+        [DllImport("user32.dll", EntryPoint = "UnhookWindowsHookEx", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool UnsetHook(IntPtr hHook);
+
+
+        [DllImport("user32.dll", EntryPoint = "CallNextHookEx", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr NextHook(IntPtr hHook, int nCode, IntPtr wParam, IntPtr lParam);
+
+
+        [DllImport("kernel32.dll", EntryPoint = "GetModuleHandle", SetLastError = true)]
+        private static extern IntPtr GetModule(String lpModuleName);
+        #endregion
+
+
+        [DllImport("user32.dll", EntryPoint = "SendInput", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern uint SendMessages(uint nInputs, INPUT[] inputs, int inpSize);
+
+        private const int
+            WM_KEYDOWN = 0x100,
+            WH_KEYBOARD_LL = 13,
+
+            WM_MOUSEMOVE = 0x200,
+            WM_LBUTTONDOWN = 0x0201,
+            WM_LBUTTONDUP = 0x0202,
+            WM_RBUTTONDOWN = 0x0204,
+            WM_RBUTTONUP = 0x0205,
+            WM_MOUSWEHEEL = 0x020A,
+            WH_MOUSE_LL = 14;
+
+        public HookForm()
+        {
+            InitializeComponent();
+        }
+
+        #region Keyboard hook
+
+        private IntPtr hKbHook;
+        private HookProc kbHookPinned;
+        private GCHandle kbGcHandle;
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct KBDLLHOOKSTRUCT
+        {
+            public int vkCode;
+            public int scanCode;
+            public int flags;
+            public int time;
+            public int dwExtraInfo;
+        }
+
+
+        private void buttonStartKb_Click(object sender, EventArgs e)
+        {
+            using (Process currentProcess = Process.GetCurrentProcess())
+            using (ProcessModule currentModule = currentProcess.MainModule)
+            {
+                if (currentModule == null) return;
+
+                kbHookPinned = KbHookProc;
+                kbGcHandle = GCHandle.Alloc(kbHookPinned);
+                hKbHook = SetHook(WH_KEYBOARD_LL, kbHookPinned, GetModule(currentModule.ModuleName), 0);
+                kbHookStatus.Text = "active";
+                kbHookStatus.ForeColor = Color.Green;
+
+            }
+        }
+
+
+
+        private void buttonStopKb_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                UnsetHook(hKbHook);
+                kbGcHandle.Free();
+                kbHookStatus.Text = "non-active";
+                kbHookStatus.ForeColor = Color.Red;
+            }
+            catch
+            {
+
+                return;
+            }
+
+
+
+
+
+        }
+
+        private IntPtr KbHookProc(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                //int keyVirtualCode = Marshal.ReadInt32(lParam);
+                var msgData = Marshal.PtrToStructure<KBDLLHOOKSTRUCT>(lParam);
+                Keys key = (Keys)msgData.vkCode;
+
+                richTextBoxKbDemo.Text += key.ToString();
+                /*if(key == Keys.LWin)
+                {
+                    richTextBoxKbDemo.Text += "(rejected)";
+                    return (IntPtr)1;
+                }*/
+
+                if (key == Keys.Space) // Key to be replaced
+                {
+                    // new system message
+                    INPUT inp = new INPUT
+                    {
+                        Type = 1 // Keyboard message
+                    };
+                    inp.Data.Keyboard.Vk = (ushort)Keys.OemMinus;
+                    inp.Data.Keyboard.Scan = 0;
+                    inp.Data.Keyboard.Flags = 0; // 0 - keyDown; 2 - keyUp
+                    inp.Data.Keyboard.Time = 0;
+                    inp.Data.Keyboard.ExtraInfo = IntPtr.Zero;
+
+
+
+                    INPUT[] msgs = new INPUT[] { inp }; // For SendInput array needed
+
+
+
+                    SendMessages(1, msgs, Marshal.SizeOf(typeof(INPUT)));
+
+                    richTextBoxKbDemo.Text += "(Space -> -)";
+
+                    // delete current message
+                    return (IntPtr)1;
+                }
+            }
+
+            return NextHook(hKbHook, nCode, wParam, lParam);
+        }
+
+        private void HookForm_Load(object sender, EventArgs e)
+        {
+            int x_res = Screen.PrimaryScreen.Bounds.Width / 10;
+            int y_res = Screen.PrimaryScreen.Bounds.Height / 10;
+            
+           
+
+            labelRes.Text = $"X: {x_res}\nY: {y_res}";
+
+
+        }
+
+
+        #endregion
+
+        #region Mouse hook
+
+        private IntPtr hMsHook;
+        private HookProc MsHookPinned;
+        private GCHandle MsGcHandle;
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct POINT
+        {
+            public int x;
+            public int y;
+
+        }
+
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct MsHookStruct
+        {
+            public POINT point;
+            public uint mouseData;
+            public uint flags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+
+        private void textBoxSourceKey_KeyDown(object sender, KeyEventArgs e)
+        {
+            e.Handled = true;
+            e.SuppressKeyPress = true;
+            if(sender is TextBox)(sender as TextBox).Text = e.KeyData.ToString();
+           
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct INPUT
+        {
+            public uint Type;
+            public MOUSEKEYBDHARDWAREINPUT Data;
+        }
+
+
+
+
+        [StructLayout(LayoutKind.Explicit)]
+        internal struct MOUSEKEYBDHARDWAREINPUT
+        {
+            [FieldOffset(0)]
+            public HARDWAREINPUT Hardware;
+            [FieldOffset(0)]
+            public KEYBDINPUT Keyboard;
+            [FieldOffset(0)]
+            public MOUSEINPUT Mouse;
+        }
+
+
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct HARDWAREINPUT
+        {
+            public uint Msg;
+            public ushort ParamL;
+            public ushort ParamH;
+        }
+
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct KEYBDINPUT
+        {
+            public ushort Vk;
+            public ushort Scan;
+            public uint Flags;
+            public uint Time;
+            public IntPtr ExtraInfo;
+        }
+
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct MOUSEINPUT
+        {
+            public int X;
+            public int Y;
+            public uint MouseData;
+            public uint Flags;
+            public uint Time;
+            public IntPtr ExtraInfo;
+        }
+
+        private IntPtr MsHookProc(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0)
+            {
+                //int keyVirtualCode = Marshal.ReadInt32(lParam);
+                var msgData = Marshal.PtrToStructure<POINT>(lParam);
+
+                switch ((int)wParam)
+                {
+                    case WM_LBUTTONDOWN: listBoxMs.Items.Add($"L{msgData.x},{msgData.y}"); break;
+                    case WM_RBUTTONDOWN: listBoxMs.Items.Add($"R{msgData.x},{msgData.y}"); break;
+                    case WM_MOUSEMOVE:
+                        var gdi = panelDisplay.CreateGraphics();
+                        using (Brush b = new SolidBrush(Color.Tomato))
+                            gdi.FillEllipse(b, msgData.x / 10, msgData.y / 10, 2, 2);
+                        break;
+
+                }
+
+            }
+
+            return NextHook(hMsHook, nCode, wParam, lParam);
+        }
+        private void buttonStartMs_Click(object sender, EventArgs e)
+        {
+            using (Process currentProcess = Process.GetCurrentProcess())
+            using (ProcessModule currentModule = currentProcess.MainModule)
+            {
+                if (currentModule == null) return;
+
+                MsHookPinned = MsHookProc;
+                MsGcHandle = GCHandle.Alloc(MsHookPinned);
+                hMsHook = SetHook(WH_MOUSE_LL, MsHookPinned, GetModule(currentModule.ModuleName), 0);
+                msHookStatus.Text = "active";
+                msHookStatus.ForeColor = Color.Green;
+
+            }
+        }
+
+        private void buttonStopMs_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MsGcHandle.Free();
+                UnsetHook(hMsHook);
+                msHookStatus.Text = "non-active";
+                msHookStatus.ForeColor = Color.Red;
+            }
+            catch
+            {
+
+                return;
+            }
+
+        }
+
+        #endregion
+    }
+
+   
+
+}
